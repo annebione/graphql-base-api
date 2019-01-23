5bf537601bcd9daf3b23dc919bfc308223762405
Anne Bione <anne@direct.one>
2019-01-23 15:47:37 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import expressGraphQL from "express-graphql";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import schema from "./graphql/";
+
+const app = express();
+const PORT = process.env.PORT || "4000";
+const db = "mongodb://leo:leoleo1@ds121753.mlab.com:21753/graphql-server";
+
+// Connect to MongoDB with Mongoose.
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
